140d8f04a7e4dbac82a251e82ea5299d8c2e0799
Nowrei <furlancedric@gmail.com>
2022-06-08 13:16:52 +0200

    card fini

--- assets/include/card.php
+++ assets/include/card.php
@@ -1,10 +1,10 @@
 <div class="row row-cols-1 row-cols-md-3 g-4 ms-5 me-5 mt-3">
   <div class="col">
     <div class="card h-100">
-      <a href="#"><img src="assets/img/539-200x200.jpg" class="card-img-top img-thumbnail" alt="responsive"></a>
+      <a href="#"><img src="assets/img/539-200x200.jpg" class="card-img-top rounded" alt="Responsive image"></a>
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h5 class="card-title">Exo 1</h5>
+        <p class="card-text">Mise en place du maquettage du one page avec redirection vers Miro et la maquette ainsi que vers GitHub avec les différents push et merge effectué.</p>
       </div>
       <div class="card-footer">
         <small class="text-muted me-auto ms-auto">
@@ -15,7 +15,7 @@
   </div>
   <div class="col">
     <div class="card h-100">
-      <a href="#"><img src="assets/img/541-200x200.jpg" class="card-img-top img-thumbnail" alt="..."></a>
+      <a href="#"><img src="assets/img/541-200x200.jpg" class="card-img-top rounded " alt="Responsive image"></a>
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -30,7 +30,7 @@
   </div>
   <div class="col">
     <div class="card h-100">
-      <a href="#"><img src="assets/img/650-200x200.jpg" class="card-img-top img-thumbnail" alt="..." ></a>
+      <a href="#"><img src="assets/img/650-200x200.jpg" class="card-img-top rounded" alt="Responsive image" ></a>
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
@@ -44,7 +44,7 @@
 <div class="row row-cols-1 row-cols-md-3 g-4 ms-5 me-5 mt-3">
   <div class="col">
     <div class="card h-100">
-      <a href="#"><img src="assets/img/730-200x200.jpg" class="card-img-top img-thumbnail" alt="..."></a>
+      <a href="#"><img src="assets/img/730-200x200.jpg" class="card-img-top rounded" alt="Responsive image"></a>
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -56,7 +56,7 @@
   </div>
   <div class="col">
     <div class="card h-100">
-      <a href="#"><img src="assets/img/826-200x200.jpg" class="card-img-top img-thumbnail" alt="..."></a>
+      <a href="#"><img src="assets/img/826-200x200.jpg" class="card-img-top rounded" alt="Responsive image"></a>
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -68,7 +68,7 @@
   </div>
   <div class="col">
     <div class="card h-100">
-      <a href="#"><img src="assets/img/1003-200x200.jpg" class="card-img-top img-thumbnail" alt="..."></a>
+      <a href="#"><img src="assets/img/1003-200x200.jpg" class="card-img-top rounded" alt="Responsive image"></a>
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
