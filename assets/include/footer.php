335414730971dc8fc9d2402b1ba4325b0a80c724
Nowrei <furlancedric@gmail.com>
2022-06-08 15:36:40 +0200

    footer mis en place

---
+++ assets/include/footer.php
@@ -0,0 +1,23 @@
+  <footer class="bg-dark text-center text-white mt-5">
+    <div class="container p-2 mt-5">
+      <section class="mb-4">
+
+        <div class="d-flex justify-content-center">
+        <div class="d-flex flex-row bd-highlight mb-3 mt-3">
+        
+  <div class="p-2 bd-highlight"> <a href="https://www.github.com" class="text-white"><h2><i
+                class="fa-brands fa-github"></i></h2></a></div>
+  <div class="p-2 bd-highlight"><a href="https://www.facebook.com/"
+              class="text-white"><h2><i class="fa-brands fa-facebook"></i></h2></a></div>
+              <div class="p-2 bd-highlight"><a href="https://www.instagram.com"
+              class="text-white"><h2><i class="fa-brands fa-instagram"></i></h2></a></div>             
+        </div>
+        </div>
+    </div>
+
+    <div class="text-center p-3">
+      © 2022 Copyright:
+      <a class="text-white">Projet 1</a>
+    </div>
+
+  </footer>
