9ea96ca842466113bf978590bfb6920f0dbf2a1a
Nowrei <furlancedric@gmail.com>
2022-06-08 11:01:58 +0200

    Mise en place href image

---
+++ assets/include/card.php
@@ -0,0 +1,81 @@
+<div class="row row-cols-1 row-cols-md-3 g-4 ms-5 me-5 mt-3">
+  <div class="col">
+    <div class="card h-100">
+      <a href="#"><img src="assets/img/539-200x200.jpg" class="card-img-top img-thumbnail" alt="responsive"></a>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted me-auto ms-auto">
+          <button type="button" class="btn btn-primary"><a class="nav-link text-light" href="https://miro.com/app/board/uXjVOvcoIII=/" target="_blank" rel="noopener noreferrer">Miro</a></button>
+        <button type="button" class="btn btn-primary"><a class="nav-link text-light" href="https://github.com/Nowrei/Exam" target="_blank" rel="noopener noreferrer">GitHub</a></button></small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <a href="#"><img src="assets/img/541-200x200.jpg" class="card-img-top img-thumbnail" alt="..."></a>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">
+        <button type="button" class="btn btn-primary"><a class="nav-link text-light" href="https://github.com" target="_blank" rel="noopener noreferrer">GitHub</a></button>
+      </small>
+        </small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <a href="#"><img src="assets/img/650-200x200.jpg" class="card-img-top img-thumbnail" alt="..." ></a>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted"><button type="button" class="btn btn-primary"><a class="nav-link text-light" href="https://github.com" target="_blank" rel="noopener noreferrer">GitHub</a></button></small>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row row-cols-1 row-cols-md-3 g-4 ms-5 me-5 mt-3">
+  <div class="col">
+    <div class="card h-100">
+      <a href="#"><img src="assets/img/730-200x200.jpg" class="card-img-top img-thumbnail" alt="..."></a>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted"><button type="button" class="btn btn-primary"><a class="nav-link text-light" href="https://github.com" target="_blank" rel="noopener noreferrer">GitHub</a></button></small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <a href="#"><img src="assets/img/826-200x200.jpg" class="card-img-top img-thumbnail" alt="..."></a>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted"><button type="button" class="btn btn-primary"><a class="nav-link text-light" href="https://github.com" target="_blank" rel="noopener noreferrer">GitHub</a></button></small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <a href="#"><img src="assets/img/1003-200x200.jpg" class="card-img-top img-thumbnail" alt="..."></a>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-muted"><button type="button" class="btn btn-primary"><a class="nav-link text-light" href="https://github.com" target="_blank" rel="noopener noreferrer">GitHub</a></button></small>
+      </div>
+    </div>
+  </div>
+</div>
